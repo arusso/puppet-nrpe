a37cd5d6610285feece954b38f43a5cc26ef3201
Aaron Russo <arusso@berkeley.edu>
2013-05-02 10:34:38 -0700

    ensure we install nrpe before reloading xinetd

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,5 +1,9 @@
 class nrpe::package {
   include nrpe
   package { 'nrpe': ensure => installed }
-  if $nrpe::xinetd_r { include xinetd }
+  if $nrpe::xinetd_r {
+    include xinetd
+    # ensure our nrpe user is setup first before we reload xinetd
+    Package['nrpe'] ~> Class['xinetd::service']
+  }
 }
