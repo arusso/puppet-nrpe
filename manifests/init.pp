dde892493f36460259edffd9fae54552b6030272
Aaron Russo <arusso@berkeley.edu>
2013-05-02 10:42:02 -0700

    ensure only commands defined in puppet exist in nrpe.d

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,18 @@ class nrpe (
 ) {
   $xinetd_r = any2bool( $xinetd )
 
+  $nrpe_dot_d_dir = '/etc/nrpe.d/'
+  # make sure we only have commands we manage in there
+  file { $nrpe_dot_d_dir:
+    ensure  => directory,
+    purge   => true,
+    force   => true,
+    recurse => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0555',
+  }
+
   if ! is_array( $allowed_hosts ) {
     $allowed_hosts_r = split($allowed_hosts, '[:,|;]')
   } else {
